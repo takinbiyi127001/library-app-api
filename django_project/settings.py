11e7cb9612f605fa2e6a1076d2281f0dc1678811
tobi.akinbiyi@aptissio.com <tobi.akinbiyi@aptissio.com>
2022-06-02 08:41:19 +1000

    Install rest

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # local
+    # 3rd-party apps
+    "rest_framework",
+    # local apps
     "books.apps.BooksConfig",
 ]
 
