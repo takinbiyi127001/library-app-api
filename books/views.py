59e1fe8a5142796ab0caca8aaa3b9d55d2fc730f
Tobi Akinbiyi <akintobiakinbiyi@gmail.com>
2022-06-01 21:34:55 +1000

    initial commit

---
+++ books/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+
+# Create your views here.
+
+from .models import Book
+
+
+class BookListView(ListView):
+    model = Book
+    template_name = "book_list.html"
+    # context_object_name = "book_list"
